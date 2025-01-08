b740bb73bf7a133b13a8684d85995bb125082542
chinmaymudholkar <chinmay.mudholkar@outlook.com>
2025-01-08 22:23:12 +0000

    Optimisation

--- NUnitPlaywrightFramework/Libs/BaseTest.cs
+++ NUnitPlaywrightFramework/Libs/BaseTest.cs
@@ -11,8 +11,8 @@ namespace NUnitPlaywrightFramework.Libs
         protected IPage _page { get; private set; }
         protected IDictionary<string, string> envVars { get; private set; }
 
-        [SetUp]
-        public async Task Setup()
+        [OneTimeSetUp]
+        public async Task OneTimeSetup()
         {
             try
             {
@@ -25,6 +25,11 @@ namespace NUnitPlaywrightFramework.Libs
                 Console.WriteLine(e.Message);
             }
             _playwright = await Playwright.CreateAsync();
+        }
+
+        [SetUp]
+        public async Task Setup()
+        {   
             _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
             _browserContext = await _browser.NewContextAsync();
             _page = await _browserContext.NewPageAsync();
@@ -36,6 +41,11 @@ namespace NUnitPlaywrightFramework.Libs
             await _page.CloseAsync();
             await _browserContext.CloseAsync();
             await _browser.CloseAsync();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTeardown()
+        {
             _playwright.Dispose();
         }
     }
