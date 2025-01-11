794a8c653311af143f3838b0b2be39c8664accf6
chinmaymudholkar <166333543+chinmaymudholkar@users.noreply.github.com>
2025-01-11 23:55:44 +0000

    Screenshots

--- NUnitPlaywrightFramework/Libs/FrameworkActions.cs
+++ NUnitPlaywrightFramework/Libs/FrameworkActions.cs
@@ -1,16 +1,21 @@
 using Microsoft.Playwright;
-
 namespace NUnitPlaywrightFramework.Libs
 {
     public class FrameworkActions
     {
         private readonly IPage _page;
+        private readonly Wrappers wrappers;
 
-        public FrameworkActions(IPage page) => _page = page;
+        public FrameworkActions(IPage page)
+        {
+            _page = page;
+            wrappers = new();
+        }
+
 
         public void PerformAction(string selector, ActionTypes action, string value)
         {
-            TakeScreenshot($"{selector}-before-{action}.png").Wait();
+            TakeScreenshot($"{selector}-before-{action}").Wait();
             switch (action)
             {
                 case ActionTypes.Click:
@@ -32,7 +37,7 @@ namespace NUnitPlaywrightFramework.Libs
                     GetTitleAsync().Wait();
                     break;
             }
-            TakeScreenshot($"{selector}-after-{action}.png").Wait();
+            TakeScreenshot($"{selector}-after-{action}").Wait();
         }
 
         public void Verify(string selector, ActionTypes property, string expectedValue)
@@ -89,7 +94,13 @@ namespace NUnitPlaywrightFramework.Libs
 
         private async Task TakeScreenshot(string file_name)
         {
-            string screenshot_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Screenshots", file_name);
+            string? base_path = string.Empty;
+            base_path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+            if (base_path == null)
+            {
+                base_path = AppDomain.CurrentDomain.BaseDirectory;
+            }
+            string screenshot_path = Path.Combine(base_path, "Screenshots", $"{wrappers.GetCurrentDateTime()}-{wrappers.CleanString(file_name)}.png");
             var options = new PageScreenshotOptions { Path = screenshot_path };
             await _page.ScreenshotAsync(options);
         }
---
+++ NUnitPlaywrightFramework/Libs/Wrappers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitPlaywrightFramework.Libs
+{
+    internal class Wrappers
+    {
+        //Create a method to get the current date and time
+        public string GetCurrentDateTime()
+        {
+            return DateTime.Now.ToString("yyyyMMddHHmmss");
+        }
+
+        public string CleanString(string input)
+        {
+            char[] charsToRemove = new char[]
+                        {
+                ' ', ':', '/', '\\', '.', ',', ';', '(', ')', '[', ']', '{', '}', '!', '@', '#', '$', '%', '^', '&', '*', '+', '=', '?', '>', '<', '|', '~', '`', '\'', '\"', '‘', '’', '“', '”'
+                        };
+
+            // Loop through the input string and remove all these characters
+            foreach (char c in charsToRemove)
+            {
+                input = input.Replace(c.ToString(), string.Empty);
+            }
+
+            // Return the cleaned string
+            return input;
+        }
+    }
+}
