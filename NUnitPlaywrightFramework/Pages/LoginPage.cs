0ecf4784810649bcead967f1e5b8d6573ab7ee57
chinmaymudholkar <166333543+chinmaymudholkar@users.noreply.github.com>
2025-01-19 11:34:08 +0000

    Optimisation

--- NUnitPlaywrightFramework/Pages/LoginPage.cs
+++ NUnitPlaywrightFramework/Pages/LoginPage.cs
@@ -5,13 +5,13 @@ namespace NUnitPlaywrightFramework.Pages
 {
     public class LoginPage
     {
-        private readonly IPage _page;
-        private readonly FrameworkActions _frameworkActions;
+        private readonly IPage page;
+        private readonly FrameworkActions frameworkActions;
 
         public LoginPage(IPage page)
         {
-            _page = page;
-            _frameworkActions = new FrameworkActions(page);
+            this.page = page;
+            frameworkActions = new FrameworkActions(page);
         }
 
         private string TxtUsername => "#user-name";
@@ -21,24 +21,24 @@ namespace NUnitPlaywrightFramework.Pages
 
         public async Task GotoAsync()
         {
-            await _page.GotoAsync("https://www.saucedemo.com/");
+            await page.GotoAsync("https://www.saucedemo.com/");
         }
 
         public void Login(string username, string password)
         {
-            _frameworkActions.PerformAction(TxtUsername, ObjectActions.Type, username);
-            _frameworkActions.PerformAction(TxtPassword, ObjectActions.Type, password);
-            _frameworkActions.PerformAction(BtnLogin, ObjectActions.Click, string.Empty);
+            frameworkActions.PerformAction(TxtUsername, ObjectActions.Type, username);
+            frameworkActions.PerformAction(TxtPassword, ObjectActions.Type, password);
+            frameworkActions.PerformAction(BtnLogin, ObjectActions.Click, string.Empty);
         }
 
         public void VerifyErrorMessage(string expectedErrorMessage)
         {
-            _frameworkActions.Verify(LblError, ObjectProperties.TEXT, expectedErrorMessage);
+            frameworkActions.Verify(LblError, ObjectProperties.TEXT, expectedErrorMessage);
         }
 
         public void VerifyTitle(string expectedTitle)
         {
-            _frameworkActions.Verify(string.Empty, ObjectProperties.TITLE, expectedTitle);
+            frameworkActions.Verify(string.Empty, ObjectProperties.TITLE, expectedTitle);
         }
     }
 }
--- NUnitPlaywrightFramework/Pages/ProductsPage.cs
+++ NUnitPlaywrightFramework/Pages/ProductsPage.cs
@@ -5,20 +5,20 @@ namespace NUnitPlaywrightFramework.Pages
 {
     public class ProductsPage
     {
-        private readonly IPage _page;
-        private readonly FrameworkActions _frameworkActions;
+        private readonly IPage page;
+        private readonly FrameworkActions frameworkActions;
 
         public ProductsPage(IPage page)
         {
-            _page = page;
-            _frameworkActions = new FrameworkActions(page);
+            this.page = page;
+            frameworkActions = new FrameworkActions(page);
         }
 
         private string LblHeading => "[data-test=\"title\"]";
 
         public void VerifyHeading(string expectedHeading)
         {
-            _frameworkActions.Verify(LblHeading, ObjectProperties.TEXT, expectedHeading);
+            frameworkActions.Verify(LblHeading, ObjectProperties.TEXT, expectedHeading);
         }
     }
 }
